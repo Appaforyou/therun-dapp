67aba841d144d27dce10d5c89a53c6cd0ca69862
Ruslan Tebyrtse <ruslan.tebyrtse@ripecode.dev>
2021-09-23 14:43:01 +0300

    Connect app to Metamask

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import {useEffect, useState} from "react";
+import Web3 from "web3";
+
 
 function App() {
+  const [accounts, setAccounts] = useState([]);
+
+  async function loadBlockchainData() {
+    if (window.ethereum) {
+      const web3 = new Web3(window.ethereum);
+      await window.ethereum.send('eth_requestAccounts');
+      const network = await web3.eth.net.getNetworkType();
+      console.log('network', network);
+      const accounts = await web3.eth.getAccounts();
+      console.log('accounts', accounts);
+      setAccounts(accounts)
+    }
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello world</h1>
+      <p>Your accounts: {accounts}</p>
     </div>
-  );
+  )
 }
 
 export default App;
