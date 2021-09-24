2d3663a24790be38562888a05ce43c4a706ae209
Ruslan Tebyrtse <ruslan.tebyrtse@ripecode.dev>
2021-09-24 10:48:54 +0300

    Replace Web3 by Ethers.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import {useEffect, useState} from "react";
-import Web3 from "web3";
-
+import {ethers} from "ethers";
 
 function App() {
-  const [accounts, setAccounts] = useState([]);
+  const [account, setAccount] = useState('');
+  const [network, setNetwork] = useState('');
+  const [balance, setBalance] = useState('');
 
   async function loadBlockchainData() {
     if (window.ethereum) {
-      const web3 = new Web3(window.ethereum);
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
       await window.ethereum.send('eth_requestAccounts');
-      const network = await web3.eth.net.getNetworkType();
-      console.log('network', network);
-      const accounts = await web3.eth.getAccounts();
-      console.log('accounts', accounts);
-      setAccounts(accounts)
+      const signer = provider.getSigner();
+      const address = await signer.getAddress();
+      setAccount(address);
+      setNetwork((await provider.getNetwork()).name);
+      setBalance((await provider.getBalance(address)).toString())
     }
   }
 
@@ -25,7 +26,9 @@ function App() {
   return (
     <div>
       <h1>Hello world</h1>
-      <p>Your accounts: {accounts}</p>
+      <p>Network: {network}</p>
+      <p>Your accounts: {account}</p>
+      <p>Your balance: {balance}</p>
     </div>
   )
 }
